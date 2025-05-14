7de573c7b101486d47acba7c950ababc0eb3b3fd
Amir Shakibafar <amir.shakibafar82@gmail.com>
2025-05-14 10:44:48 +0330

    added hero section

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,63 @@
+import Search from "./components/Search";
+import { useState, useEffect } from "react";
+
+const API_BASE_URL = "https://api.themoviedb.org/3";
+const API_KEY = import.meta.env.VITE_TMDB_API_KEY;
+const API_OPTIONS = {
+  method: "GET",
+  headers: {
+    accept: "application/json",
+    Authorization: `Bearer ${API_KEY}`,
+  },
+};
 function App() {
+  const [searchTerm, setSearchTerm] = useState("");
+  const [errorMessage, setErrorMessage] = useState("");
+  const [movies, setMovies] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+
+  
+  const fetchMovies = async () => {
+    try {
+      const endPoint = `${API_BASE_URL}/discover/movie?sort_by=popularity.desc`;
+      const response = await fetch(endPoint, API_OPTIONS );
+      if (!response.ok) {
+        throw new Error("response was not ok")
+
+      }
+      const data = await response.json();
+      if (data.response === "False") {
+        throw new Error("failed to fetch movies")
+      }
+      console.log(data);
+      
+    } catch (e) {
+      console.log(e);
+      setErrorMessage("error while fetchingg movies please try again later.");
+    }
+  };
+  useEffect(() => {
+    fetchMovies();
+  }, []);
   return (
-      <h1 className="bg-amber-600 text-8xl">Hello world</h1>
-  )
+    <main>
+      <div className="pattern"></div>
+      <div className="wrapper">
+        <header>
+          <img src="/hero.png" alt="" />
+          <h1>
+            Find <span className="text-gradient">Movies</span> You'll Enjoy
+            Without The Hassle
+          </h1>
+          <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
+        </header>
+        <section className="all-movies">
+          <h2>All Movies</h2>
+          {!!errorMessage && <p className="error-message">{errorMessage}</p>}
+        </section>
+      </div>
+    </main>
+  );
 }
 
-export default App
+export default App;
